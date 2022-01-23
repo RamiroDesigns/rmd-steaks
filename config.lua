4ea3880c3d473667f258a1e907009f9f5f8a230b
RamiroDesigns <87922196+RamiroDesigns@users.noreply.github.com>
2022-01-23 14:52:42 -0600

    initial commit

--- config.lua
+++ config.lua
@@ -1,11 +1,10 @@
 Config = {}
 
-
 Config.ButchTime = 8000 -- 8Seconds
 Config.CookTime = 10000 -- 10Seconds
 Config.RawAmount = math.random(2,4) -- can be changed to what ever amount
 
-Config.Props = { -- Object Hash used to Cook Meats
+Config.Props = { -- BBQ Pit Objects Hashes
     1903501406,
     286252949,
 }
